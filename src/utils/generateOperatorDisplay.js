05fdfcde3f6df802977bff77a106c7d013ef7759
sformichella <sformichella@gmail.com>
2021-07-20 09:57:18 -0700

    small refactor

--- src/utils/generateOperatorDisplay.js
+++ src/utils/generateOperatorDisplay.js
@@ -16,15 +16,6 @@ export const generateOperatorDisplayFromView = (operator, operandsState) => {
   })
 }
 
-const addStyleToHTML = (html, style) => {
-  return `
-    <style>
-      ${style}
-    </style>
-    ${html}
-  `
-}
-
 const operatorValueStyle = `
   .operator {
     display: flex;
@@ -38,7 +29,29 @@ const operatorValueStyle = `
 `
 
 export const generateOperatorDisplayFromValue = (operator, operandsState) => {
-  const [operandsString, operatorString] = operator.value
+  // Use operator string to generate HTML string
+  const operatorHTML = htmlFromOperatorString(operator.value)
+
+  // Add basic styling
+  const view = addStyleToHTML(operatorHTML, operatorValueStyle);
+
+  // Parse styled html string to React and replace divs 
+  // that have 'operand' name attribute with  the Operand Selector
+  return ReactHTMLParser(view, {
+    transform: node => {
+      if (node.attribs?.name === 'operand') {
+        return <SelectOperand 
+          id={node.attribs.id} 
+          operandsState={operandsState}
+        />
+      }
+    }
+  })
+}
+
+// Utility for generateOperatorDisplayFromValue
+function htmlFromOperatorString(operator) {
+  const [operandsString, operatorString] = operator
     .split('=>')
     .map(elem => elem.trim())
 
@@ -48,7 +61,7 @@ export const generateOperatorDisplayFromValue = (operator, operandsState) => {
 
   // Wrap operands and other string elements in
   // divs
-  const wrappedOperands = operands
+  const wrappedOperatorString = operands
     .reduce((view, operand) => {
       return view.replace(operand, `__${operand}__`)
     }, operatorString)
@@ -64,20 +77,14 @@ export const generateOperatorDisplayFromValue = (operator, operandsState) => {
     })
     .join('')
 
-  // Put everything in a container
-  const operatorHTML = `<div class="operator">${wrappedOperands}</div>`
+  return `<div class="operator">${wrappedOperatorString}</div>`;
+}
 
-  // Parse html string to React and replace divs 
-  // that have 'operand' name attribute with 
-  // the Operand Selector
-  return ReactHTMLParser(addStyleToHTML(operatorHTML, operatorValueStyle), {
-    transform: node => {
-      if (node.attribs?.name === 'operand') {
-        return <SelectOperand 
-          id={node.attribs.id} 
-          operandsState={operandsState}
-        />
-      }
-    }
-  })
+function addStyleToHTML (html, style) {
+  return `
+    <style>
+      ${style}
+    </style>
+    ${html}
+  `
 }
