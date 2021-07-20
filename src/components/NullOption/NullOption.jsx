a7f6193d7b5352a14c6628a1990319662eaa52d8
sformichella <sformichella@gmail.com>
2021-07-20 09:35:21 -0700

    comment

--- src/components/NullOption/NullOption.jsx
+++ src/components/NullOption/NullOption.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default function NullOption({ nullSwitch, children }) {
+  // children should be a string; no extra jsx
   return (
     nullSwitch 
     ? <option value={''}>
